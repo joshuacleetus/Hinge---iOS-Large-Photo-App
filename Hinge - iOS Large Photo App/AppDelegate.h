63b0dae87bf6b536a4ce85a3ace13e45de0ad601
Joshua Cleetus <joshua.cleetus@gmail.com>
2015-10-19 22:30:23 -0400

    Initial Commit

---
+++ Hinge - iOS Large Photo App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hinge - iOS Large Photo App
+//
+//  Created by Joshua on 10/19/15.
+//  Copyright © 2015 joshuacleetus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hinge - iOS Large Photo App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hinge - iOS Large Photo App
+//
+//  Created by Joshua on 10/19/15.
+//  Copyright © 2015 joshuacleetus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
